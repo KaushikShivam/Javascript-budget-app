1bbe3c63503c943c1f3312ee2b0d70ed75d56f51
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-04-26 11:06:14 +0530

    Set up initial event listeners

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 var budgetController = (function() {
-	// var x = 23;
-	// var add = function(a) {
-	// 	return x + a;
-	// };
 	//code
 })();
 
@@ -11,5 +7,19 @@ var UIController = (function() {
 })();
 
 var controller = (function(budgetCtrl, UIctrl) {
-	//code
+	var ctrlAddItem = function() {
+		//1. Get inout data
+		//2. Add item to the budget controller
+		//3. Add the new item to the UI controller
+		//4. Calculate the budget
+		//5. Display the budget
+	};
+
+	document.querySelector('.add__button').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event) {
+		if (event.keyCode === 13 || event.which === 13) {
+			ctrlAddItem();
+		}
+	});
 })(budgetController, UIController);
